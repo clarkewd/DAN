0b64be41ae6514023850dc1647cc4ec397d32d7b
liuhalei <liuhalei@gmail.com>
2012-10-22 11:59:10 +0200

    menu text in two lines, equal width, and responsive

--- scripts/my-script.js
+++ scripts/my-script.js
@@ -53,6 +53,7 @@ $(function(){ //jQuery Document Ready
 	$(window).resize(function(){
 		windowHeight();
 		hideFooter();
+		contentPositionInt();
 		googlemapResize ();
 	});
 	/* windows scroll function */
@@ -94,10 +95,10 @@ $(function(){ //jQuery Document Ready
 		var navHeight = $('.site-nav').height();
 		var heightOffset = 32;
 		// $('.page-content-page-focus .wrap .display-container').css('margin-top', navHeight+heightOffset);
-		$('.mobile-top-push').css('height', navHeight)
+		$('.mobile-top-push').css('height', navHeight);
 		if(Modernizr.mq('screen and (max-width: 480px)')){
 			// $('.page-content-page-focus .wrap .display-container').css('margin-top', '0px');
-			$('.mobile-top-push').css('height', '0px')
+			$('.mobile-top-push').css('height', '0px');
 		}
 	}
 	function bookingScoll(){
